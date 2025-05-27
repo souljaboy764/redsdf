a02ac4d7b58c05f9c333a7d279f69fdb6d32d41f
souljaboy764 <6441764+souljaboy764@users.noreply.github.com>
2025-05-27 17:59:35 +0200

    additionally visualizing ground-truth mesh

--- examples/static_manifold/plot_model.py
+++ examples/static_manifold/plot_model.py
@@ -2,9 +2,11 @@ import torch
 import argparse
 import numpy as np
 import redsdf.utils as utils
+import open3d as o3d
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_file', type=str, default="checkpoint.pt", help="path of trained model")
+parser.add_argument('--mesh_file', type=str, default="", help="path of ground truth mesh model")
 parser.add_argument('--use_cuda', action="store_true", default=False, help="whether to use cuda")
 args = parser.parse_args()
 
@@ -18,7 +20,13 @@ pcl, _ = utils.generate_pointcloud_by_raymarching(dis_model, pose_c, device=devi
                                                             [-45, -45, 0], [-135, -45, 0], [45, 45, 0], [135, 45, 0],
                                                             [-45, 45, 0], [-135, 45, 0]],
                                                   epsilon=0.0005)
-utils.create_vis_animation(pcl)
+
+if args.mesh_file!="":
+    mesh = o3d.io.read_triangle_mesh(args.mesh_file)
+    mesh.compute_vertex_normals()
+else:
+    mesh = None
+utils.create_vis_animation(pcl, mesh)
 # utils.visualization_pointcloud(pcl)
-utils.plot2Dcontour(dis_model, pose_c, axis="z", v=0, device=device)
-utils.plot2Dcontour(dis_model, pose_c, axis="y", v=-0.25, device=device)
+# utils.plot2Dcontour(dis_model, pose_c, axis="z", v=0, device=device)
+# utils.plot2Dcontour(dis_model, pose_c, axis="y", v=-0.25, device=device)
